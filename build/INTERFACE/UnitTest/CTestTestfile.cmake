11020f3459f322047c2ff494960c2a76077030e8
KeziahAK <kez.kgs@gmail.com>
2022-12-07 16:28:54 +0100

    bqnqne

--- build/INTERFACE/UnitTest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/keziahapaloo-kingslove/Documents/IMAC/Semestre_3/Projet_info_maths/libduturfu/Rationnels/UnitTest
-# Build directory: /Users/keziahapaloo-kingslove/Documents/IMAC/Semestre_3/Projet_info_maths/libduturfu/build/INTERFACE/UnitTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/keziahapaloo-kingslove/Documents/IMAC/Semestre_3/Projet_info_maths/libduturfu/build/INTERFACE/UnitTest/UnitTest[1]_include.cmake")
--- build/INTERFACE/UnitTest/UnitTest[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[RationnelsConstructor.basicConstructor]=]  /Users/keziahapaloo-kingslove/Documents/IMAC/Semestre_3/Projet_info_maths/libduturfu/build/bin/UnitTest [==[--gtest_filter=RationnelsConstructor.basicConstructor]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[RationnelsConstructor.basicConstructor]=]  PROPERTIES WORKING_DIRECTORY /Users/keziahapaloo-kingslove/Documents/IMAC/Semestre_3/Projet_info_maths/libduturfu/build/INTERFACE/UnitTest SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  UnitTest_TESTS RationnelsConstructor.basicConstructor)
