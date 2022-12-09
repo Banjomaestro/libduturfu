42d7de3c03376a898a37b9d8a3a9a647d27713c3
Banjomaestro <benji.amrani@gmail.com>
2022-12-09 14:55:36 +0100

    badadida

---
+++ build/INTERFACE/UnitTest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mac/Documents/lessons/imac/second year/first semester/libduturfu/Rationnels/UnitTest
+# Build directory: /Users/mac/Documents/lessons/imac/second year/first semester/libduturfu/build/INTERFACE/UnitTest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/mac/Documents/lessons/imac/second year/first semester/libduturfu/build/INTERFACE/UnitTest/UnitTest[1]_include.cmake")
